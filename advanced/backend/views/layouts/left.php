1f562d64c7474c78dfc98fa8f42c88bc91b263d1
unknown <741715116@qq.com>
2017-09-26 15:49:32 +0800

    修改主页显示

--- advanced/backend/views/layouts/left.php
+++ advanced/backend/views/layouts/left.php
@@ -1,7 +1,5 @@
 <aside class="main-sidebar">
-
     <section class="sidebar">
-
         <!-- Sidebar user panel -->
         <div class="user-panel">
             <div class="pull-left image">
@@ -13,7 +11,6 @@
                 <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
             </div>
         </div>
-
         <!-- search form -->
         <form action="#" method="get" class="sidebar-form">
             <div class="input-group">
@@ -35,27 +32,19 @@
                 'label' => $menu['name'], 
                 'url' => [$menu['route']], 
             ]; 
-
-            //处理我们的配置 
             if ($data) { 
-                //visible 
                 isset($data['visible']) && $return['visible'] = $data['visible']; 
-                //icon 
                 isset($data['icon']) && $data['icon'] && $return['icon'] = $data['icon']; 
-                //other attribute e.g. class... 
                 $return['options'] = $data; 
             } 
-            //没配置图标的显示默认图标 
             (!isset($return['icon']) || !$return['icon']) && $return['icon'] = 'fa fa-circle-o'; 
             $items && $return['items'] = $items;
             return $return; 
         }; 
-        //这里我们对一开始写的菜单menu进行了优化
         echo Menu::widget([
             'options' => ['class' => 'sidebar-menu'],
             'items' => MenuHelper::getAssignedMenu(Yii::$app->user->id, null, $callback),
         ]); 
         ?>
     </section>
-
 </aside>
